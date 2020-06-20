adf99a043d1de32255efe5023b299741daff619d
testUser <shubh_tiwari@hotmail.com>
2020-06-20 15:23:01 +0530

    Swift UI basic concept includes : 1. Creating Label 2. Adding VStack 3. Adding HStack 4. Use of Spacer()

---
+++ Swift-UI-01/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  Swift-UI-01
+//
+//  Created by Shobhakar Tiwari on 20/06/20.
+//  Copyright © 2020 shobhakar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            Spacer()
+            HStack{
+                Spacer()
+                Text("Swift-UI")
+                Spacer()
+                Text("Swift-UI")
+                Spacer()
+                Text("Swift-UI")
+                Spacer()
+            }
+            Spacer()
+            HStack{
+                       Spacer()
+                       Text("Swift-UI")
+                       Spacer()
+                       Text("Swift-UI")
+                       Spacer()
+                       Text("Swift-UI")
+                       Spacer()
+                   }
+            Spacer()
+            HStack{
+                Spacer()
+                Text("Swift-UI")
+                Spacer()
+                Text("Swift-UI")
+                Spacer()
+                Text("Swift-UI")
+                Spacer()
+            }
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
